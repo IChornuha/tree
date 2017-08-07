2ce39141d25532f6946ba77c6edd9b3db3be01d7
IChornuha <igor.chornuha@gmail.com>
2017-08-07 17:39:56 +0300

    Fix autoload - BASE_PATH const added

--- __autoload.php
+++ __autoload.php
@@ -1,4 +1,5 @@
 <?php
+define('BASE_PATH', realpath(dirname(__FILE__)));
 spl_autoload_register(function ($class) {
     $filename = BASE_PATH. '/' . str_replace('\\', '/', $class) . '.php';
     include($filename);
